78a805f229d7cb772ad87a795dcd004e50d70176
GithubSim13 <144484430+GithubSim13@users.noreply.github.com>
2024-05-28 12:51:44 +0800

    Update index.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,17 +12,15 @@ const About = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="mb-12 max-w-[540px] lg:mb-0">
                 <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-                  Brilliant Toolkit to Build Next.js SaaS Websites.
+                  Company Overview
                 </h2>
                 <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
-                  The main thrust is to focus on educating attendees on how to
-                  best protect highly vulnerable business applications with
-                  interactive panel discussions and roundtables led by subject
-                  matter experts.
+                Jiu Studios is a tech consulting agency that works with visionaries around the world
+                 to help build software solutions that solve real world problems. 
                   <br /> <br />
-                  The main thrust is to focus on educating attendees on how to
-                  best protect highly vulnerable business applications with
-                  interactive panel.
+                  We focus on radical, yet strategic, product development through various services that
+                   include AI/ML, Web, and Software Development, UX/UI Design, and SEO Optimization. We 
+                   focus on driving growth and scaling businesses and startups through lean and efficient strategies.
                 </p>
 
                 <a
