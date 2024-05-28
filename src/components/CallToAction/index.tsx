88667abc1594b31e06c724e3024a2f25fa4b1b56
GithubSim13 <144484430+GithubSim13@users.noreply.github.com>
2024-05-28 12:51:09 +0800

    Update index.tsx

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -7,12 +7,11 @@ const CallToAction = () => {
         <div className="relative overflow-hidden">
           <div className="-mx-4 flex flex-wrap items-stretch">
             <div className="w-full px-4">
-              <div className="mx-auto max-w-[570px] text-center">
+              <div className="mx-auto max-w-[1000px] text-center">
                 <h2 className="mb-2.5 text-3xl font-bold text-white md:text-[38px] md:leading-[1.44]">
                   <span>Ready to scale your business to Unicorn Status?</span>
                   <p className="text-3xl font-normal md:text-[40px]">
-                    {" "}
-                    Let's Get To It!{" "}
+                    {" "}Let's Get To It!{" "}
                   </p>
                 </h2>
                 <p className="mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-white">
