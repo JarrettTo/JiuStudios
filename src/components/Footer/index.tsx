e528598bdf26a1af96b39a66c17a8a4c8a2260cd
Jose Simbillo <144484430+GithubSim13@users.noreply.github.com>
2024-05-31 13:49:38 +0800

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -138,39 +138,39 @@ const Footer = () => {
           <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-3/12 xl:w-2/12">
             <div className="mb-10 w-full">
               <h4 className="mb-9 text-lg font-semibold text-white">
-                Features
+                Affiliates
               </h4>
               <ul>
                 <li>
                   <a
-                    href="/#"
+                    href="https://www.matchstix.ph"
                     className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
                   >
-                    How it works
+                    Matchstix
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/#"
+                    href="https://www.thebuzz.ph"
                     className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
                   >
-                    Privacy policy
+                    The Buzz
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/#"
+                    href="https://www.neuralle.com"
                     className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
                   >
-                    Terms of Service
+                    Neuralle
                   </a>
                 </li>
                 <li>
                   <a
-                    href="/#"
+                    href="https://www.sbai.com"
                     className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
                   >
-                    Refund policy
+                    SBAI
                   </a>
                 </li>
               </ul>
